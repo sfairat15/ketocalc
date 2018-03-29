2f96a5f414c3ebe5e85a24bc3727b4cba7712269
Renat <sfairat@gmail.com>
2018-03-29 12:57:16 +0300

    текст о Программе

--- app/src/main/java/ru/profitcode/ketocalc/AboutActivity.java
+++ app/src/main/java/ru/profitcode/ketocalc/AboutActivity.java
@@ -2,6 +2,7 @@ package ru.profitcode.ketocalc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        TextView version = findViewById(R.id.about_version);
+        version.setText(version.getResources().getString(R.string.about_version, BuildConfig.VERSION_NAME));
     }
 }
