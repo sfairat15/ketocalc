f28bbc4e0bdfc2968bf163502f918e60d61e2a93
rencodev <sfairat@gmail.com>
2018-06-07 17:32:04 +0300

    Добавил рекламу в about

--- app/src/main/java/ru/profitcode/ketocalc/AboutActivity.java
+++ app/src/main/java/ru/profitcode/ketocalc/AboutActivity.java
@@ -4,14 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class AboutActivity extends AppCompatActivity {
+public class AboutActivity extends BaseAdvActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
 
         TextView version = findViewById(R.id.about_version);
         version.setText(version.getResources().getString(R.string.about_version, BuildConfig.VERSION_NAME));
+    }
+
+    @Override
+    protected int getLayoutResourceId() {
+        return R.layout.activity_about;
     }
 }
