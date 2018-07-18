7051d682c6e6e7aa0e85b25accc81a9ee572f59b
Renat <sfairat@gmail.com>
2018-07-18 18:28:19 +0300

    Добавил видео с ютуба

--- app/src/main/java/ru/profitcode/ketocalc/AboutProActivity.java
+++ app/src/main/java/ru/profitcode/ketocalc/AboutProActivity.java
@@ -3,18 +3,28 @@ package ru.profitcode.ketocalc;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class AboutProActivity extends BaseBannerAdvActivity {
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerFragment;
+
+public class AboutProActivity extends BaseBannerAdvActivity implements YouTubePlayer.OnInitializedListener {
+
+    private YouTubePlayerFragment playerFragment;
+    private YouTubePlayer mPlayer;
+    private String YouTubeKey = "AIzaSyBzWve6WqjAl9u9qd_OiZvjsz9tc-O56zI";
+    private Button mInstallPro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Button installPro = findViewById(R.id.install_pro_btn);
-        installPro.setOnClickListener(new View.OnClickListener() {
+        mInstallPro = findViewById(R.id.install_pro_btn);
+        mInstallPro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_VIEW);
@@ -22,21 +32,23 @@ public class AboutProActivity extends BaseBannerAdvActivity {
 
                 try {
                     startActivity(intent);
-                }
-                catch(Exception e) {
+                } catch (Exception e) {
                     try {
                         intent.setData(Uri.parse("https://play.google.com/store/apps/details?id=ru.profitcode.ketocalcpropaid&referrer=utm_source%3Dfreeversion%26utm_medium%3Dbutton"));
                         startActivity(intent);
-                    }
-                    catch (Exception ex)
-                    {
+                    } catch (Exception ex) {
                         Toast.makeText(v.getContext(),
-                                v.getResources().getString(R.string.install_pro_error,  e.getMessage()),
+                                v.getResources().getString(R.string.install_pro_error, e.getMessage()),
                                 Toast.LENGTH_LONG).show();
                     }
                 }
             }
         });
+
+        playerFragment =
+                (YouTubePlayerFragment) getFragmentManager().findFragmentById(R.id.youtube_player_fragment);
+
+        playerFragment.initialize(YouTubeKey, this);
     }
 
     @Override
@@ -47,5 +59,41 @@ public class AboutProActivity extends BaseBannerAdvActivity {
     @Override
     protected String getAdUnitId() {
         return "ca-app-pub-9772487056729057/4337561452";
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player,
+                                        boolean wasRestored) {
+        mPlayer = player;
+
+        //Enables automatic control of orientation
+        mPlayer.setFullscreenControlFlags(YouTubePlayer.FULLSCREEN_FLAG_CONTROL_ORIENTATION);
+
+        //Show full screen in landscape mode always
+        mPlayer.addFullscreenControlFlag(YouTubePlayer.FULLSCREEN_FLAG_ALWAYS_FULLSCREEN_IN_LANDSCAPE);
+
+        //System controls will appear automatically
+        mPlayer.addFullscreenControlFlag(YouTubePlayer.FULLSCREEN_FLAG_CONTROL_SYSTEM_UI);
+
+        if (!wasRestored) {
+
+            //show thumbnail
+            //mPlayer.cueVideo("GqQNQlfxqtY");
+
+            //instead of URL https://www.youtube.com/watch?reload=9&v=GqQNQlfxqtY&feature=youtu.be
+            //we should use only v parameter value
+            //start playing at activity start
+            mPlayer.loadVideo("GqQNQlfxqtY");
+        }
+        else
+        {
+            mPlayer.play();
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                        YouTubeInitializationResult errorReason) {
+        mPlayer = null;
     }
 }
